557697a14201b352299eceb623e648be7653b18f
BaskerPrabakaran <baskerprabakarand@gmail.com>
2023-11-03 21:07:49 +0530

    feature LNP P002
    Updated html file for design issue
    Added content to load language preference
    Updated css file for design footer
    Updated js file to handle language selection.
    Added json file for different language

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 window.jQuery || document.write("<script src=\"js/lib/jquery-3.7.1.js\"><script>")
 
 $(function(){
+    
+    loadpagecontent();
+
     $("li").click(function(){
         $("li.timeline-year-active").removeClass("timeline-year-active");
         $(this).addClass("timeline-year-active");
@@ -18,4 +21,42 @@ $(function(){
             $("#navlinks").addClass("nav-links-open");
         }
     });
+    
+    $("#language-en").click(function(){
+        changeLanguage("lang-en");
+    });
+    $("#language-tm").click(function(){
+        changeLanguage("lang-tm");
+    });
 });
+
+async function loadpagecontent()
+{
+    var lang = localStorage.getItem("language");
+    if(lang == null) lang = "lang-tm";
+
+    setLanguagePreference(lang);
+    const langData = await fetchLanguageData(lang);
+    updateContent(langData);
+}
+
+async function changeLanguage(lang) {
+    await setLanguagePreference(lang);
+    location.reload();
+}
+
+function setLanguagePreference(lang) {
+    localStorage.setItem("language", lang);
+}
+
+async function fetchLanguageData(lang) {
+    const response = await fetch(`./data/${lang}.json`);
+    return response.json();
+}
+
+function updateContent(langData) {
+    document.querySelectorAll('[data-i18n]').forEach(element => {
+        const key = element.getAttribute('data-i18n');
+        element.innerHTML = langData[key];
+    });
+}
